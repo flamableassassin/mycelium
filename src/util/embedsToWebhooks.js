c1f3d8ca86e6043a2f99754bb54f9806528aa039
flammableassassin <43076116+flamableassassin@users.noreply.github.com>
2022-09-12 19:11:31 +0100

    renaming embedToWebhook to better reflect what it does

--- src/util/embedsToWebhooks.js
+++ src/util/embedsToWebhooks.js
@@ -1,18 +1,18 @@
 const Webhook = require('./Webhook');
 /**
 * Adds embeds to a webhook or it creates a new webhook if it doesn't exist
-* @param {import("../util/Embed")[]} embeds
-* @param {import("../util//Webhook")[]} webhooks
+* @param {import("./Embed")[]} embeds
+* @param {import("./Webhook")[]} webhooks
 * @param {string} name // The name of the webhook name
 * @param {string} [avatar] // The avatar for the webhook
-* @returns {import("../util/Webhook")[]} 
+* @returns {import("./Webhook")[]} 
 */
 module.exports = (embeds, webhooks, name, avatar = undefined) => {
 
   // Splitting the embeds which share the same url into their own array 
   // This is done to prevent the tweets from going across webhooks, keeping the same order while using as few webhooks as possible
 
-  /** @type {import("../util/Embed")[][]} */
+  /** @type {import("./Embed")[][]} */
   const collections = [];
   for (let i = 0; i < embeds.length; i++) {
     const index = collections.findIndex(arr => arr[0]?.url === embeds[i].url);
